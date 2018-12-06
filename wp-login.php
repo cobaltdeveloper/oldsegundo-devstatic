632bc116ccdf32d25212ec60ac72cf3e0a492e2d
Christopher <43961806+cobaltdeveloper@users.noreply.github.com>
2018-12-06 08:29:11 -0600

    Refactor static files

---
+++ wp-login.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-US">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Log In &lsaquo; Old Segundo Productions &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<script type='text/javascript' src='/wp-admin/load-scripts.php?c=0&amp;load%5B%5D=jquery-core,jquery-migrate&amp;ver=4.9.8'></script>
+<link rel='stylesheet' href='/wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.8' type='text/css' media='all' />
+<link rel='stylesheet' id='widgetkit-admin-css'  href='/wp-content/plugins/widgetkit-for-elementor/admin/assets/css/admin.css?ver=4.9.8' type='text/css' media='all' />
+<link rel='stylesheet' id='widgetkit-sweetalert2-css-css'  href='/wp-content/plugins/widgetkit-for-elementor/admin/assets/css/sweetalert2.min.css?ver=4.9.8' type='text/css' media='all' />
+<meta name='robots' content='noindex,follow' />
+	<meta name="viewport" content="width=device-width" />
+		</head>
+	<body class="login login-action-login wp-core-ui  locale-en-us">
+		<div id="login">
+		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">Powered by WordPress</a></h1>
+	
+<form name="loginform" id="loginform" action="/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Username or Email Address<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Password<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+		<input type="hidden" name="redirect_to" value="/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="/wp-login.php?action=lostpassword">Lost your password?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="/">&larr; Back to Old Segundo Productions</a></p>
+		
+	</div>
+
+	
+	<script type='text/javascript' src='/wp-admin/load-scripts.php?c=0&amp;load%5B%5D=jquery-ui-core,jquery-ui-widget,jquery-ui-tabs&amp;ver=4.9.8'></script>
+<script type='text/javascript' src='/wp-content/plugins/widgetkit-for-elementor/admin/assets/js/admin.js?ver=1.0'></script>
+<script type='text/javascript' src='/wp-content/plugins/widgetkit-for-elementor/admin/assets/js/core.js?ver=1.0'></script>
+<script type='text/javascript' src='/wp-content/plugins/widgetkit-for-elementor/admin/assets/js/sweetalert2.min.js?ver=1.0'></script>
+	<div class="clear"></div>
+	</body>
+	</html>
+	
